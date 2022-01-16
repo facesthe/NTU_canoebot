127b7171f967552ea3021ea91778566557af42bd
cruzerngz <61926974+cruzerngz@users.noreply.github.com>
2022-01-16 21:51:48 +0800

    moved formscraper cli to new file
    formscraper.py will now contain functions that fetch and manipulate the resulting json to represent the google form

--- formscraper/formscraper.py
+++ formscraper/formscraper.py
@@ -1,34 +1,27 @@
 import requests as rq
 import re
 from lxml import html
+import json
 
-form_id = input('Paste the form id here: ')
-file_name = input('Enter the outfile name: ')
 
-url = f'https://docs.google.com/forms/d/e/{form_id}/viewform'
+def get_raw_form_json(form_id:str):
+    url = f'https://docs.google.com/forms/d/e/{form_id}/viewform'
+    response = rq.get(url)
+    tree = html.fromstring(response.content)
+    script = tree.xpath('//script[@type="text/javascript"]/text()') \
+        ## successful in obtaining script section of html
 
-print(f'requesting data from {url} ...')
-response = rq.get(url)
-tree = html.fromstring(response.content)
+    jsonstr = None ## init default
 
-script = tree.xpath('//script[@type="text/javascript"]/text()') ## successful in obtaining script section of html
+    for item in script:
+        if 'var FB_PUBLIC_LOAD_DATA' in item:
+            # print(item)
+            jsonstr = item
+            break
 
-jsonstr = None
+    ## format json properly
+    jsonstr = re.sub(r'^.*=\s', '', jsonstr) ## remove variable name
+    jsonstr = re.sub(r';', '', jsonstr) ## remove semicolon
 
-for item in script:
-    if 'var FB_PUBLIC_LOAD_DATA' in item:
-        # print(item)
-        jsonstr = item
-        break
+    return json.loads(jsonstr)
 
-if jsonstr == None:
-    print('No form data found.')
-    exit(-1)
-
-## format into a json string
-jsonstr = re.sub(r'^.*=\s', '', jsonstr)
-jsonstr = re.sub(r';', '', jsonstr)
-
-with open(f'{file_name}.json', 'w') as f:
-    f.write(jsonstr)
-print(f'form fields written to {file_name}.json')
---
+++ formscraper/formscraper.test.py
@@ -0,0 +1,4 @@
+import formscraper as fs
+
+
+
