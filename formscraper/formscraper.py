613befbc7a468ed91f61fc18857bc82d04286f39
cruzerngz <61926974+cruzerngz@users.noreply.github.com>
2022-01-18 09:46:12 +0800

    formscraper formatting done
    stable

--- formscraper/formscraper.py
+++ formscraper/formscraper.py
@@ -2,6 +2,24 @@ import requests as rq
 import re
 from lxml import html
 import json
+import copy
+
+## template JSON for form information
+sampleJSON = {
+    "title": None,
+    "description": None,
+    "formid": None,
+    "fields": None, ## None or list of samplefields
+}
+
+## template JSON for form field information
+## optional as some fields are open-ended)
+samplefield = {
+    "name": None,
+    "id": None,
+    "options": None ## None or list
+}
+
 
 def get_raw_json(form_id):
     '''Produces the raw script json from a specified form.\n
@@ -27,3 +45,40 @@ def get_raw_json(form_id):
 
     return json.loads(jsonstr)
 
+
+def format_raw_form(jsonobj):
+    ## assigning to JSON
+    rawfields = copy.deepcopy(jsonobj[1][1])
+    returnJSON = sampleJSON
+    returnJSON['title'] = jsonobj[3]
+    returnJSON['description'] = jsonobj[1][0]
+    returnJSON['formid'] = re.sub(r'^.*/', '', jsonobj[14])
+    returnJSON['fields'] = [copy.deepcopy(samplefield) for i in range(len(rawfields))]
+
+    ## assigning fields
+    offset = 0
+    for i in range(len(rawfields)):
+        print(rawfields[i][1])
+        returnJSON['fields'][i+offset]['name'] = rawfields[i][1]
+
+
+        try: ## attempt to get field id
+            returnJSON['fields'][i+offset]['id'] = rawfields[i][4][0][0]
+        except: ## attempt failed, index does not correspond to a field (question), skip
+            print('indexing of None occured, skipping current index')
+            returnJSON['fields'].pop(i+offset)
+            offset -= 1
+            continue
+
+        # print(rawfields[i][4][0][0])
+        # print(returnJSON['fields'][i]['id'])
+
+        # assigining field options (if any)
+        if rawfields[i][4][0][1] is not None:
+            returnJSON['fields'][i+offset]['options'] = \
+                [rawfields[i][4][0][1][j][0] for \
+                j in range(len(rawfields[i][4][0][1]))]
+
+
+
+    return returnJSON
--- formscraper/formscraper.test.py
+++ formscraper/formscraper.test.py
@@ -1,7 +1,14 @@
 import pandas as pd
 import formscraper
+import json
 
-jsonobj = formscraper.get_raw_json('1FAIpQLScblJJuHMBwCx-iK230jlxNxCVOPOauHjkQLMBkhdH1KG9OBA')
+jsonobj = formscraper.get_raw_json('1FAIpQLSefOwdgq9W4HJGNy2aa8q5oRnj3wHx4mOtGHcE7yhIkM_x2Pw') ## ITCC
 
-print(jsonobj)
+# jsonobj = formscraper.get_raw_json('1FAIpQLScblJJuHMBwCx-iK230jlxNxCVOPOauHjkQLMBkhdH1KG9OBA') ## hall X
+
+
+jsonformatted = formscraper.format_raw_form(jsonobj)
+
+with open('itcc-availability-newformatted.json','w') as f:
+    f.write(json.dumps(jsonformatted))
 
