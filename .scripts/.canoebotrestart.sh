cbae69a4639eaf95a17c017e33aa6d8ff89b5668
cruzerngz <61926974+cruzerngz@users.noreply.github.com>
2022-02-15 15:36:19 +0000

    update bash scripts

--- .scripts/.canoebotrestart.sh
+++ .scripts/.canoebotrestart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo kill 15 $(pgrep python3)
 cd /home/pi/canoebot ##dirname needs to change
-nohup python3 canoebot.py &
+nohup python3 canoebot.py > /dev/null 2>&1 &
 sleep 1
 echo
--- settings.py
+++ settings.py
@@ -5,8 +5,8 @@ import lib.Dotionary as dot
 
 ## path to settings file - do not change!
 ## modify the json file to change settings
-# _path = './.settings/botsettings.json' ## deployed version
-_path = './.settings/botsettings.debug.json' ## debug version
+# _path = './.configs/botsettings.json' ## deployed version
+_path = './.configs/botsettings.debug.json' ## debug version
 
 with open(_path) as jsonfile:
     json = jsn.load(jsonfile)
