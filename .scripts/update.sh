2827ce79f2d309a07cf5b6c72fa47745c6a9fa66
cruzerngz <cruzerngz+github@gmail.com>
2022-02-16 22:07:27 +0800

    Update update.sh

--- .scripts/update.sh
+++ .scripts/update.sh
@@ -4,3 +4,5 @@ source functions.sh # import functions
 source .repopath.sh # import repopath
 
 git_shallow_pull $repopath
+echo "remember to edit settings.py to load botsettings.json!"
+echo "defaults to botsettings.debug.json"
