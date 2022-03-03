d80622499a2ab2c9d48e37d8e1a644634bd933fa
cruzerngz <61926974+cruzerngz@users.noreply.github.com>
2022-03-03 17:29:55 +0800

    Update .canoebotupdate.sh
    double shallow pull to bring HEAD to most recent and be on latest commit

--- .scripts/.canoebotupdate.sh
+++ .scripts/.canoebotupdate.sh
@@ -12,5 +12,6 @@ bold=$(tput bold)
 rst=$(tput sgr0)
 
 git_shallow_pull $repopath
+git_shallow_pull $repopath
 echo "$red""$bold""remember to switch to the correct telegram bot!""$rst"
 echo "$red""defaults to botsettings.debug.json""$rst"
