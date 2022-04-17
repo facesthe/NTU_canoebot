b8777af3d020ec86aa95cc2ab1ee614f9c8d6b2e
cruzerngz <61926974+cruzerngz@users.noreply.github.com>
2022-04-17 21:54:31 +0800

    Readme manual
    create detailed instructions on installation and use.
    create getUpdates file to read updates data when bot is not polling

---
+++ getupdates.py
@@ -0,0 +1,22 @@
+import settings as s
+import requests as rq
+import json as jsn
+
+updates_url = f"https://api.telegram.org/bot{s.json.canoebot.apikey}/getUpdates"
+
+send_msg_url = f"https://api.telegram.org/bot{s.json.canoebot.apikey}/sendMessage"
+
+print(f"using config file {s._path}")
+
+
+get_params = {
+    "offset": None
+}
+response:rq.Response = rq.get(updates_url)
+
+empty_post_response:rq.Response = rq.post(send_msg_url, {})
+
+response_json = jsn.loads(response.text)
+print(jsn.dumps(response_json, indent=4))
+print("Post response:")
+print(empty_post_response)
