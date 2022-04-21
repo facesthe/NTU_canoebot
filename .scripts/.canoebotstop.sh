0f4df21897cb9290b928e84914dad78b48c001ee
cruzerngz <61926974+cruzerngz@users.noreply.github.com>
2022-04-21 15:01:28 +0800

    kill command fix
    forgot the -flag

--- .scripts/.canoebotstop.sh
+++ .scripts/.canoebotstop.sh
@@ -7,6 +7,6 @@ repopath=$(realpath .)
 ## enter venv
 source $repopath/.venv/bin/activate
 
-sudo kill $(pgrep -f "python3 canoebot.py")
+sudo kill -15 $(pgrep -f "python3 canoebot.py")
 echo "canoebot stopped"
 deactivate
