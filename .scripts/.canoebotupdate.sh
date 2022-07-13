69bea589eca3c87c83a9c2dfa9c69054acaf45e4
cruzerngz <cruzerngz+github@gmail.com>
2022-07-13 18:57:58 +0800

    fix scripts bug + new dependency
    - add "schedule" dependency for executing periodic events
    - fix functions in echo_colours.sh
    - add update command for pip3 in .canoebotupdate.sh

--- .scripts/.canoebotupdate.sh
+++ .scripts/.canoebotupdate.sh
@@ -11,6 +11,11 @@ red=$(tput setaf 1)
 bold=$(tput bold)
 rst=$(tput sgr0)
 
+## update pip3 modules
+source $repopath/.venv/bin/activate
+pip3 install -r $repopath/.scripts/requirements.txt
+deactivate
+
 git_shallow_pull $repopath
 git_shallow_pull $repopath
 echo "$red""$bold""remember to switch to the correct telegram bot!""$rst"
