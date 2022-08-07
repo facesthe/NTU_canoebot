b392563aefa9fbcd617322b16abbebd8ad59f91a
cruzerngz <cruzerngz+github@gmail.com>
2022-08-07 22:03:02 +0800

    Create install_venv.sh
    install_venv is used for development work. Installs relevant packages for the project.

---
+++ .scripts/install_venv.sh
@@ -0,0 +1,23 @@
+# get the absolute repo path (not the .scripts path!)
+# return to original path (wherever script was called)
+currpath=$(realpath .)
+cd $(dirname $(realpath "$0")) && cd ..
+repopath=$(realpath .)
+cd $currpath
+
+source $repopath/.scripts/functions.sh # import functions
+source $repopath/.scripts/data.sh # cron and aliases stored here
+source $repopath/.scripts/echo_colours.sh # colour to console
+
+# create virtual environment
+echo_green "creating python3 virtual environent (venv)..."
+python3 -m venv $repopath/.venv
+echo_green "virtual environment created. Entering venv..."
+source $repopath/.venv/bin/activate
+
+# install pip3 packages
+echo_green "installing python3 dependencies..."
+pip3 install -r $repopath/.scripts/requirements.txt
+
+## exit venv
+deactivate
