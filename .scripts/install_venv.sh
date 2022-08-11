8e98715779dbad4767d13ee027547eab70910798
cruzerngz <cruzerngz+github@gmail.com>
2022-08-11 14:59:20 +0800

    Update install_venv.sh
    add bash shebang, remove unnecessary includes

--- .scripts/install_venv.sh
+++ .scripts/install_venv.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # get the absolute repo path (not the .scripts path!)
 # return to original path (wherever script was called)
 currpath=$(realpath .)
@@ -5,8 +7,8 @@ cd $(dirname $(realpath "$0")) && cd ..
 repopath=$(realpath .)
 cd $currpath
 
-source $repopath/.scripts/functions.sh # import functions
-source $repopath/.scripts/data.sh # cron and aliases stored here
+# source $repopath/.scripts/functions.sh # import functions
+# source $repopath/.scripts/data.sh # cron and aliases stored here
 source $repopath/.scripts/echo_colours.sh # colour to console
 
 # create virtual environment
@@ -16,8 +18,8 @@ echo_green "virtual environment created. Entering venv..."
 source $repopath/.venv/bin/activate
 
 # install pip3 packages
-echo_green "installing python3 dependencies..."
-pip3 install -r $repopath/.scripts/requirements.txt
+echo_green "installing/updating python3 dependencies..."
+pip3 install -r $repopath/.scripts/requirements.txt --upgrade
 
 ## exit venv
 deactivate
